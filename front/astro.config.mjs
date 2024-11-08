f7a15b622e8efb7d701385f00fca0f4ef9f8b618
Rubirojo05 <rubenrojomoreno@gmail.com>
2024-11-08 12:22:33 +0100

    Commit Astro

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://rubennrouge.tech', // Reemplaza con tu URL de GitHub Pages
+});
+
