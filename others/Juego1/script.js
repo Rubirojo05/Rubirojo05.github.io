871254ca453f599a4a263230d8b3fe914d06b107
Rubii <75586703+Rubirojo05@users.noreply.github.com>
2024-11-25 12:40:59 +0100

    Update script.js

--- others/Juego1/script.js
+++ others/Juego1/script.js
@@ -5,7 +5,7 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from
 
 const firebaseConfig = {
     apiKey: "AIzaSyD2KJ0N0FksQJl658h-HdvkAO8CsLue1vw",
-    authDomain: "rubennrouge.tech",
+    authDomain: "juego1-ca38d.firebaseapp.com",
     projectId: "juego1-ca38d",
     storageBucket: "juego1-ca38d.appspot.com",
     messagingSenderId: "416427181010",
