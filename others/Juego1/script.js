3494feee9b0e6575232e481173377bc32e4b493e
Rubii <75586703+Rubirojo05@users.noreply.github.com>
2024-11-25 12:10:08 +0100

    Update script.js

--- others/Juego1/script.js
+++ others/Juego1/script.js
@@ -1,11 +1,11 @@
 // Firebase Configuración
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getFirestore, doc, getDoc, setDoc, collection, query, orderBy, limit, getDocs } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
-import { getAuth, onAuthStateChanged, setPersistence, browserLocalPersistence, signOut } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { getFirestore, collection, query, orderBy, limit, getDoc, setDoc, doc, getDocs } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyD2KJ0N0FksQJl658h-HdvkAO8CsLue1vw",
-    authDomain: "rubennrouge.tech",
+    authDomain: "juego1-ca38d.firebaseapp.com",
     projectId: "juego1-ca38d",
     storageBucket: "juego1-ca38d.appspot.com",
     messagingSenderId: "416427181010",
@@ -16,76 +16,139 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
-// Configurar persistencia de sesión
-setPersistence(auth, browserLocalPersistence)
-    .catch((error) => {
-        console.error("Error al configurar la persistencia de sesión:", error);
-    });
-
+// Variables globales
 let userId = null;
-let saldo = 1500;
+let saldo = 1500; // Establecer saldo inicial en 1.500€
 let nombreUsuario = null;
 let nombreCambiado = sessionStorage.getItem('nombreCambiado') === 'true';
 
+// Desactivar botón de girar antes de iniciar sesión
 document.getElementById('botonGirar').disabled = true;
+
+// Referencia al botón de cerrar sesión
 const botonCerrarSesion = document.getElementById('botonCerrarSesion');
 
-// Verificar autenticación y autorización cuando el estado cambia
+// Verificar el estado de autenticación
 onAuthStateChanged(auth, async (user) => {
     if (user) {
         userId = user.uid;
-
-        // Verificar si el usuario tiene un token válido
-        try {
-            const idToken = await user.getIdToken();
-            console.log('Token del usuario:', idToken); // Verificación adicional
-
-            // Verificar si el usuario está autorizado en la base de datos
-            const userDocMantenimiento = await getDoc(doc(db, "authorizedUsers", userId));
-            if (!userDocMantenimiento.exists() || !userDocMantenimiento.data().authorized) {
-                await signOut(auth);
-                window.location.href = '/others/Mantenimiento/index.html';
-                return;
-            }
-
-            // Obtener datos del ranking
-            const userDoc = await getDoc(doc(db, "ranking", userId));
-            if (userDoc.exists()) {
-                saldo = userDoc.data().saldo;
-                nombreCambiado = userDoc.data().nombreCambiado || nombreCambiado;
-                nombreUsuario = userDoc.data().nombre || user.displayName || "Anónimo";
-            } else {
-                await setDoc(doc(db, "ranking", userId), { nombre: user.displayName || "Anónimo", saldo, nombreCambiado });
-                nombreUsuario = user.displayName || "Anónimo";
-            }
-
-            actualizarSaldo();
-            obtenerRanking();
-
-            // Mostrar elementos y habilitar botón
-            document.getElementById('botonLogin').style.display = 'none';
-            document.getElementById('botonGirar').disabled = false;
-            document.querySelector('.cambiar-nombre').style.display = nombreCambiado ? 'none' : 'block';
-            botonCerrarSesion.style.display = 'block';
-
-        } catch (error) {
-            console.error("Error al obtener el token:", error);
-            window.location.href = '/others/Mantenimiento/index.html';
+        const userDoc = await getDoc(doc(db, "ranking", userId));
+        if (userDoc.exists()) {
+            saldo = userDoc.data().saldo;
+            nombreCambiado = userDoc.data().nombreCambiado || nombreCambiado;
+            nombreUsuario = userDoc.data().nombre; // Asegurarse de recuperar el nombre guardado
+        } else {
+            await agregarJugador(user.displayName || "Anónimo", saldo);
+            nombreUsuario = user.displayName || "Anónimo";
         }
+
+        localStorage.setItem('nombreUsuario', nombreUsuario);
+        actualizarSaldo();
+        obtenerRanking();
+        document.getElementById('botonLogin').style.display = 'none';
+        document.getElementById('botonGirar').disabled = false;
+
+        if (!nombreCambiado) {
+            document.querySelector('.cambiar-nombre').style.display = 'block';
+        } else {
+            document.querySelector('.cambiar-nombre').style.display = 'none';
+        }
+
+        botonCerrarSesion.style.display = 'block';
     } else {
-        // Si el usuario no está autenticado, redirigir al login de mantenimiento
-        window.location.href = '/others/Mantenimiento/index.html';
+        document.getElementById('botonLogin').style.display = 'block';
+        document.querySelector('.cambiar-nombre').style.display = 'none';
+        botonCerrarSesion.style.display = 'none';
+    }
+});
+
+// Iniciar sesión con Google
+document.getElementById('botonLogin').addEventListener('click', async () => {
+    try {
+        const result = await signInWithPopup(auth, provider);
+        const user = result.user;
+        userId = user.uid;
+        const userDoc = await getDoc(doc(db, "ranking", userId));
+        if (userDoc.exists()) {
+            saldo = userDoc.data().saldo;
+            nombreCambiado = userDoc.data().nombreCambiado || nombreCambiado;
+            nombreUsuario = userDoc.data().nombre; // Asegurarse de recuperar el nombre guardado
+        } else {
+            await agregarJugador(user.displayName || "Anónimo", saldo);
+            nombreUsuario = user.displayName || "Anónimo";
+        }
+
+        localStorage.setItem('nombreUsuario', nombreUsuario);
+        actualizarSaldo();
+        obtenerRanking();
+        document.getElementById('botonLogin').style.display = 'none';
+        document.getElementById('botonGirar').disabled = false;
+
+        if (!nombreCambiado) {
+            document.querySelector('.cambiar-nombre').style.display = 'block';
+        } else {
+            document.querySelector('.cambiar-nombre').style.display = 'none';
+        }
+
+        botonCerrarSesion.style.display = 'block';
+    } catch (error) {
+        console.error("Error al iniciar sesión con Google:", error);
     }
 });
 
 // Evento de cerrar sesión
-botonCerrarSesion.addEventListener('click', async () => {
-    await signOut(auth);
-    sessionStorage.clear();
-    localStorage.clear();
-    window.location.href = '/others/Mantenimiento/index.html';
+botonCerrarSesion.addEventListener('click', () => {
+    auth.signOut().then(() => {
+        sessionStorage.clear();
+        localStorage.removeItem('nombreUsuario');
+        window.location.reload();
+    }).catch((error) => {
+        console.error('Error al cerrar sesión:', error);
+    });
 });
+
+// Actualizar saldo
+function actualizarSaldo() {
+    const saldoElemento = document.getElementById('saldo');
+    saldoElemento.textContent = `Saldo: ${saldo}€`;
+    document.getElementById('botonGirar').disabled = saldo <= 0;
+}
+
+// Obtener ranking
+async function obtenerRanking() {
+    const rankingContainer = document.getElementById('ranking');
+    const q = query(collection(db, "ranking"), orderBy("saldo", "desc"), limit(5));
+    const querySnapshot = await getDocs(q);
+
+    rankingContainer.innerHTML = '';
+    let posicion = 1;
+    querySnapshot.forEach((doc) => {
+        const jugador = doc.data();
+        if (jugador.nombre && jugador.saldo !== undefined) {
+            const div = document.createElement('div');
+            div.textContent = `${posicion}. ${jugador.nombre}: ${jugador.saldo}€`;
+            rankingContainer.appendChild(div);
+            posicion++;
+        }
+    });
+}
+
+// Agregar jugador al ranking
+async function agregarJugador(nombre, saldo) {
+    if (!nombre || nombre.trim() === "" || /[^a-zA-Z0-9]/.test(nombre) || nombre.length > 20) {
+        mostrarAviso("Por favor, introduce un nombre válido (sin caracteres especiales, máximo 20 caracteres).");
+        return;
+    }
+
+    try {
+        await setDoc(doc(db, "ranking", userId), { nombre, saldo, nombreCambiado }, { merge: true });
+        obtenerRanking();
+    } catch (error) {
+        console.error("Error al guardar el jugador:", error);
+    }
+}
 
 // Función para obtener un símbolo aleatorio
 function obtenerSimboloAleatorio() {
@@ -93,7 +156,7 @@ function obtenerSimboloAleatorio() {
     return simbolos[Math.floor(Math.random() * simbolos.length)];
 }
 
-// Función para girar los carretes
+// Girar los carretes
 function girarCarretes() {
     if (saldo <= 0) {
         mostrarAviso("Saldo insuficiente. Recarga para seguir jugando.");
@@ -101,31 +164,35 @@ function girarCarretes() {
     }
     saldo -= 50;
     actualizarSaldo();
-    agregarJugador(nombreUsuario || "Anónimo", saldo);
+    agregarJugador(nombreUsuario || user.displayName || "Anónimo", saldo);
 
-    document.getElementById('botonGirar').disabled = true;
-    const mensaje = document.getElementById('mensaje');
+    botonGirar.disabled = true;
     mensaje.textContent = "Girando...";
-    const carretes = ['carrete1', 'carrete2', 'carrete3'].map(id => document.getElementById(id));
     let giros = 20;
     let intervalo = setInterval(() => {
-        carretes.forEach(carrete => carrete.textContent = obtenerSimboloAleatorio());
+        carrete1.textContent = obtenerSimboloAleatorio();
+        carrete2.textContent = obtenerSimboloAleatorio();
+        carrete3.textContent = obtenerSimboloAleatorio();
         giros--;
         if (giros === 0) {
             clearInterval(intervalo);
-            determinarResultado(carretes);
-            document.getElementById('botonGirar').disabled = saldo <= 0;
+            determinarResultado();
+            botonGirar.disabled = saldo <= 0;
             mensaje.textContent = "¡Presiona 'Girar' para comenzar!";
         }
     }, 100);
 }
 
 // Determinar resultado de los carretes
-function determinarResultado([carrete1, carrete2, carrete3]) {
-    const [simbolo1, simbolo2, simbolo3] = [carrete1.textContent, carrete2.textContent, carrete3.textContent];
+function determinarResultado() {
+    const simbolo1 = carrete1.textContent;
+    const simbolo2 = carrete2.textContent;
+    const simbolo3 = carrete3.textContent;
+
     if (simbolo1 === simbolo2 && simbolo2 === simbolo3) {
-        saldo += 500;
-        mostrarAviso(`¡Felicidades! Ganaste 500€`);
+        const premio = 500;
+        saldo += premio;
+        mostrarAviso(`¡Felicidades! Ganaste ${premio}€`);
     } else {
         mostrarAviso("¡Inténtalo de nuevo!");
     }
@@ -140,26 +207,24 @@ function mostrarAviso(texto) {
     setTimeout(() => aviso.classList.remove("mostrar"), 3000);
 }
 
-// Actualizar saldo
-function actualizarSaldo() {
-    document.getElementById('saldo').textContent = `Saldo: ${saldo}€`;
-}
+// Evento de girar
+document.getElementById('botonGirar').addEventListener('click', girarCarretes);
 
-// Obtener ranking
-async function obtenerRanking() {
-    const rankingContainer = document.getElementById('ranking');
-    const q = query(collection(db, "ranking"), orderBy("saldo", "desc"), limit(5));
-    const querySnapshot = await getDocs(q);
-
-    rankingContainer.innerHTML = '';
-    querySnapshot.forEach((doc, index) => {
-        const jugador = doc.data();
-        rankingContainer.innerHTML += `<div>${index + 1}. ${jugador.nombre}: ${jugador.saldo}€</div>`;
-    });
-}
-
-// Agregar jugador al ranking
-async function agregarJugador(nombre, saldo) {
-    await setDoc(doc(db, "ranking", userId), { nombre, saldo, nombreCambiado }, { merge: true });
-    obtenerRanking();
-}
+// Cambiar nombre
+document.getElementById('botonCambiarNombre').addEventListener('click', async () => {
+    nombreUsuario = document.getElementById('nombreUsuario').value.trim();
+    if (nombreUsuario && !/[^a-zA-Z0-9]/.test(nombreUsuario) && nombreUsuario.length <= 20) {
+        if (saldo >= 1000) {
+            saldo -= 1000;
+            actualizarSaldo();
+            nombreCambiado = true;
+            sessionStorage.setItem('nombreCambiado', 'true');
+            await agregarJugador(nombreUsuario, saldo);
+            localStorage.setItem('nombreUsuario', nombreUsuario);
+            document.querySelector('.cambiar-nombre').style.display = 'none';
+            mostrarAviso('Nombre cambiado exitosamente.');
+        } else {
+            mostrarAviso('Saldo insuficiente para cambiar el nombre.');
+        }
+    }
+});
